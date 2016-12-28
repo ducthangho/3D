b543c64045a43c97867581e77a3a12bfe7ce0c33
zzunio <zzunio@gmail.com>
2016-12-28 10:41:50 +0700

    lenh gen proto
    vd:  ygrpc yservice     // trong do yservice la ten file proto trong thu
    muc scheme

---
+++ y3d/bin/yproto.bat
@@ -0,0 +1,9 @@
+%~d0
+cd %Y3D_ROOT%\bin\scheme
+
+%Y3D_ROOT%\bin\protoc -I=. --proto_path=%Y3D_ROOT%\Service\grpc\third_party\protobuf\src --cpp_out=. %1.proto
+%Y3D_ROOT%\bin\protoc -I=. --proto_path=%Y3D_ROOT%\Service\grpc\third_party\protobuf\src --csharp_out=. %1.proto
+
+move /y *.cs %Y3D_ROOT%\y3d\Models\
+move /y *.h %Y3D_ROOT%\Service\
+move /y *.cc %Y3D_ROOT%\Service\
