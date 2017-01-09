4235eaf66bca915029f546e0c7f0d4b3879ffd22
ducthangho <ducthangho@gmail.com>
2017-01-09 20:23:35 +0700

    Fix update_src to point to v1.0.s

--- y3d/Service/update_src.cmd
+++ y3d/Service/update_src.cmd
@@ -3,14 +3,16 @@ REM xcopy .gitmodules grpc /D /Y
 xcopy .gitignore grpc /D /Y
 cd grpc
 git fetch
-git checkout v1.0.1
-git submodule update --recursive --remote
+git checkout v1.0.s
+git submodule update --init --recursive
+git submodule update --remote
 ) else (
-git clone --depth 1 -b 'v1.0.1' --single-branch https://github.com/grpc/grpc.git
+git clone --depth 1 --branch v1.0.s --single-branch https://github.com/grpc/grpc.git
 REM xcopy .gitmodules grpc/ /D /Y
 xcopy .gitignore grpc /Y
 cd grpc
-git submodule update --init --recursive --remote
+git submodule update --init --recursive
+git submodule update --remote
 REM git config core.sparseCheckout true
 REM echo src/* >> .git/info/sparse-checkout
 REM echo include/* >> .git/info/sparse-checkout
