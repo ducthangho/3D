2a308ed7015a9b2b689aa27eadd588c32ae53e75
ducthangho <ducthangho@gmail.com>
2017-02-09 01:40:33 +0700

    Adjust build scripts. Add a post_build event script

--- y3d/bin/ygen.cmd
+++ y3d/bin/ygen.cmd
@@ -4,3 +4,5 @@ call %Y3D_ROOT%\bin\yproto y3d
 call %Y3D_ROOT%\bin\yproto xnormal
 call %Y3D_ROOT%\bin\yproto ymat
 call %Y3D_ROOT%\bin\yproto ysetting
+call %Y3D_ROOT%\bin\ygrpc yloader
+call %Y3D_ROOT%\bin\yproto yloader
--- y3d/bin/yproto.bat
+++ y3d/bin/yproto.bat
@@ -1,12 +1,12 @@
 %~d0
 cd %Y3D_ROOT%\bin\scheme
 
-%Y3D_ROOT%\bin\protoc -I=. --proto_path=%Y3D_ROOT%\Service\grpc\third_party\protobuf\src --cpp_out=. %1.proto
+%Y3D_ROOT%\bin\protoc -I=. --proto_path=%Y3D_ROOT%\Service\grpc\third_party\protobuf\src --cpp_out=dllexport_decl=Y3D_EXPORT_MACRO:. %1.proto
 %Y3D_ROOT%\bin\protoc -I=. --proto_path=%Y3D_ROOT%\Service\grpc\third_party\protobuf\src --csharp_out=. %1.proto
 
 xcopy /y *.cs %Y3D_ROOT%\..\MaxNet\Y3D\Y3D.Entities\
 move /y *.cs %Y3D_ROOT%\y3d\Models\
-move /y *.h %Y3D_ROOT%\Service\
-move /y *.cc %Y3D_ROOT%\Service\
+move /y *.h %Y3D_ROOT%\include\
+move /y *.cc %Y3D_ROOT%\ServiceImpl\
 
 cd %Y3D_ROOT%\bin\
