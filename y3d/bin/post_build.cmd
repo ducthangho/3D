02c2257985c5048797cb7ed80d5a680b5b96a5e4
ducthangho <ducthangho@gmail.com>
2017-02-09 15:12:52 +0700

    Fix post_build.cmd

--- y3d/bin/post_build.cmd
+++ y3d/bin/post_build.cmd
@@ -1,5 +1,5 @@
 rem %1\..\x64\Release\LoaderCmd.exe -l 0
-echo xcopy %1\..\lib\grpc\*.dll %2 /D /Y
+rem echo xcopy %1\..\lib\grpc\*.dll %2 /D /Y
 xcopy %1\..\lib\grpc\*.dll %2 /D /Y
-echo %1\..\bin\LoaderCmd.exe -l 1
+rem echo %1\..\bin\LoaderCmd.exe -l 1
 %1\..\bin\LoaderCmd.exe -l 1
