99c08ad484be61f8d71de83d41cf7bd48f8b950d
ducthangho <ducthangho@gmail.com>
2017-02-09 15:11:22 +0700

    Update Service, remove all dependent to 3ds Max

--- y3d/Service/DllEntry.cpp
+++ y3d/Service/DllEntry.cpp
@@ -15,7 +15,6 @@
 #include "Y3dService.h"
 #include "YLibs.h"
 
-extern ClassDesc2* GetServiceDesc();
 
 HINSTANCE hInstance;
 int controlsInit = FALSE;
@@ -54,56 +53,6 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL,ULONG fdwReason,LPVOID /*lpvReserved*/)
 	return(TRUE);
 }
 
-// This function returns a string that describes the DLL and where the user
-// could purchase the DLL if they don't have it.
-__declspec( dllexport ) const TCHAR* LibDescription()
-{
-	return GetString(IDS_LIBDESCRIPTION);
-}
-
-// This function returns the number of plug-in classes this DLL
-//TODO: Must change this number when adding a new class
-__declspec( dllexport ) int LibNumberClasses()
-{
-	return 1;
-}
-
-// This function returns the number of plug-in classes this DLL
-__declspec( dllexport ) ClassDesc* LibClassDesc(int i)
-{
-	switch(i) {
-		case 0: return GetServiceDesc();
-		default: return 0;
-	}
-}
-
-// This function returns a pre-defined constant indicating the version of 
-// the system under which it was compiled.  It is used to allow the system
-// to catch obsolete DLLs.
-__declspec( dllexport ) ULONG LibVersion()
-{
-	return VERSION_3DSMAX;
-}
-
-// This function is called once, right after your plugin has been loaded by 3ds Max. 
-// Perform one-time plugin initialization in this method.
-// Return TRUE if you deem your plugin successfully loaded, or FALSE otherwise. If 
-// the function returns FALSE, the system will NOT load the plugin, it will then call FreeLibrary
-// on your DLL, and send you a message.
-__declspec( dllexport ) int LibInitialize(void)
-{
-	#pragma message(TODO("Perform initialization here."))
-	return TRUE;
-}
-
-// This function is called once, just before the plugin is unloaded. 
-// Perform one-time plugin un-initialization in this method."
-// The system doesn't pay attention to a return value.
-__declspec( dllexport ) int LibShutdown(void)
-{
-	#pragma message(TODO("Perform un-initialization here."))
-	return TRUE;
-}
 
 TCHAR *GetString(int id)
 {
--- y3d/Service/Y3dService.cpp
+++ y3d/Service/Y3dService.cpp
@@ -12,29 +12,20 @@
 // AUTHOR: 
 //***************************************************************************/
 
-#include "Loader.h"
 #include "Y3dService.h"
-#include "maxscript/maxscript.h"
 #include <iostream>
 #include <memory>
 #include <string>
 #include <grpc++/grpc++.h>
 
 #include "YCServer.h"
-#include <iparamb2.h>
-#include <utilapi.h>
 #include <grpc++/impl/grpc_library.h>
 #include <grpc++/impl/codegen/proto_utils.h>
-#include <plugin.h>
 
 #define Service_CLASS_ID	Class_ID(0x768455e0, 0x74aca221)
 const std::string LOADER_IP = "0.0.0.0:50050";
 const std::string SERVER_IP = "0.0.0.0:50051";
 static const char *dll_fname = "SBMExporter.dle";
-
-typedef int(*PluginInitFunc)();
-typedef int(*PluginShutdownFunc)();
-typedef ClassDesc *(*PluginClassDescFunc)(int);
 
 HMODULE dll = nullptr;
 std::promise<void> exit_requested;
@@ -45,50 +36,6 @@ std::atomic<bool> isShuttingdown = false;
 std::condition_variable shutdown_cv;
 
 
-//__declspec(dllimport) int Initialize(void* codegen, void* gli);
-
-class Y3dService : public GUP
-{
-public:
-	//Constructor/Destructor
-	Y3dService();
-	virtual ~Y3dService();
-
-	// GUP Methods
-	virtual DWORD     Start();
-	virtual void      Stop();
-	virtual DWORD_PTR Control(DWORD parameter);
-	virtual void      DeleteThis();
-
-	// Loading/Saving
-	virtual IOResult Save(ISave* isave);
-	virtual IOResult Load(ILoad* iload);
-};
-
-
-
-
-class ServiceClassDesc : public ClassDesc2 
-{
-public:
-	virtual int IsPublic() 							{ return TRUE; }
-	virtual void* Create(BOOL /*loading = FALSE*/);
-	virtual const TCHAR *	ClassName() 			{ return GetString(IDS_CLASS_NAME); }
-	virtual SClass_ID SuperClassID() 				{ return GUP_CLASS_ID; }
-	virtual Class_ID ClassID() 						{ return Service_CLASS_ID; }
-	virtual const TCHAR* Category() 				{ return GetString(IDS_CATEGORY); }
-
-	virtual const TCHAR* InternalName() 			{ return _T("Y3dService"); }	// returns fixed parsable name (scripter-visible name)
-	virtual HINSTANCE HInstance() 					{ return hInstance; }					// returns owning module handle
-	
-
-};
-
-
-ClassDesc2* GetServiceDesc() { 
-	static ServiceClassDesc ServiceDesc;
-	return &ServiceDesc; 
-}
 
 DLLAPI void APIENTRY startService(const char* dllname /*= "ServiceImpl.dll"*/)
 {
@@ -228,91 +175,3 @@ DLLAPI void APIENTRY stopService()
 	
 
 }
-
-Y3dService::Y3dService()
-{
-
-}
-
-Y3dService::~Y3dService()
-{
-}
-
-void Y3dService::DeleteThis()
-{
-	delete this;
-}
-
-// Activate and Stay Resident
-DWORD Y3dService::Start()
-{
-	#pragma message(TODO("Do plugin initialization here"))
-	#pragma message(TODO("Return if you want remain loaded or not"))
-	/*grpc::internal::GrpcLibraryInitializer g_gli_initializer;
-	g_gli_initializer.summon();*/	
-	mprintf(L"Starting YService for 3ds Max ...\n");	
-	mprintf(L"Server listening on 0.0.0.0:50051\n");
-		
-	std::thread t1([]() {
-
-		/*initSystem();
-		registerCB();*/
-		//MessageBox(NULL, _T("Open the message box "), _T("message"), MB_OK | MB_SYSTEMMODAL);		
-		/*if (grpc::g_core_codegen_interface == nullptr) {
-		grpc::g_core_codegen_interface = &grpc::internal::g_core_codegen;
-		}*/
-		DbgAssert(grpc::g_glip != nullptr);
-		DbgAssert(grpc::g_core_codegen_interface != nullptr);
-		std::string server_address(LOADER_IP);
-		//ServicePtr service = getServiceImpl();			
-		auto servicePtr = getLoaderServicePtr();
-		servicePtr->Initialize(grpc::g_core_codegen_interface, grpc::g_glip);//This is a must have function, used to initialize static global function of grpc
-		grpc::ServerBuilder builder;
-		builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-		// register "service" as the instance through which we'll communicate with
-		// clients. in this case it corresponds to an *synchronous* service.
-		builder.RegisterService(servicePtr.get());
-		// finally assemble the server.
-		std::unique_ptr<Server> server(builder.BuildAndStart());
-
-		//std::cout << "server listening on " << server_address << std::endl;		
-		// wait for the server to shutdown. note that some other thread must be
-		// responsible for shutting down the server for this call to ever return.
-		//do {
-		try {
-			//service->Helloworld();
-			server->Wait();
-		}
-		catch (std::exception& e) {
-			std::wstring s = s2ws(fmt::format("Exception {}\n", e.what()));
-			MessageBox(NULL, s.c_str(), L"Exception", MB_OK);
-		};
-		//releaseObject(service);
-		//} while (1);
-		//*/
-	});
-	t1.detach();
-	return GUPRESULT_KEEP;
-}
-
-void Y3dService::Stop()
-{
-	#pragma message(TODO("Do plugin un-initialization here"))
-}
-
-DWORD_PTR Y3dService::Control( DWORD /*parameter*/ )
-{
-	return 0;
-}
-
-IOResult Y3dService::Save(ISave* /*isave*/)
-{
-	return IO_OK;
-}
-
-IOResult Y3dService::Load(ILoad* /*iload*/)
-{
-	return IO_OK;
-}
-
-inline void * ServiceClassDesc::Create(BOOL) { return new Y3dService(); }
