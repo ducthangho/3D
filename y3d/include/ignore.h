4039f57b8b2953a7b38dc723fe327182a5dc3743
ducthangho <ducthangho@gmail.com>
2017-02-10 17:05:56 +0700

    Add missing commit

--- y3d/include/ignore.h
+++ y3d/include/ignore.h
@@ -1,5 +1,6 @@
 #pragma once
 #if !defined(Y3D_EXPORT_MACRO) || Y3D_EXPORT_MACRO >= 0
+#undef Y3D_EXPORT_MACRO
 #define Y3D_EXPORT_MACRO 
 #define Y3D_EXPORT_GLOBAL_MACRO 
 //printf("hello defined world")
