5f9feb69c716537586f969a313e6b27b746b5d54
ducthangho <ducthangho@gmail.com>
2017-02-10 10:02:28 +0700

    Merge proto  files

--- y3d/bin/post_build.cmd
+++ y3d/bin/post_build.cmd
@@ -1,5 +1,6 @@
 rem %1\..\x64\Release\LoaderCmd.exe -l 0
 rem echo xcopy %1\..\lib\grpc\*.dll %2 /D /Y
 xcopy %1\..\lib\grpc\*.dll %2 /D /Y
+rem echo %ERRORLEVEL%
 rem echo %1\..\bin\LoaderCmd.exe -l 1
 %1\..\bin\LoaderCmd.exe -l 1
