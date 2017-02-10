b94722cb69db5fca11012b9722e9ae843a5b5009
ducthangho <ducthangho@gmail.com>
2017-02-10 14:34:25 +0700

    Fix post_build.cmd

--- y3d/bin/post_build.cmd
+++ y3d/bin/post_build.cmd
@@ -1,6 +1,13 @@
 rem %1\..\x64\Release\LoaderCmd.exe -l 0
-rem echo xcopy %1\..\lib\grpc\*.dll %2 /D /Y
-xcopy %1\..\lib\grpc\*.dll %2 /D /Y
+echo xcopy %1..\lib\grpc\*.dll %2 /D /Y
+xcopy %1..\lib\grpc\*.dll %2 /D /Y
+xcopy %1..\lib\grpc\*.pdb %2 /D /Y
+echo xcopy %1..\lib\protobuf\*.dll %2 /D /Y
+xcopy %1..\lib\protobuf\*.dll %2 /D /Y
+xcopy %1..\lib\protobuf\*.pdb %2 /D /Y
+echo xcopy %1..\lib\tbb\*.dll %2 /D /Y
+xcopy %1..\lib\tbb\*.dll %2 /D /Y
+xcopy %1..\lib\tbb\*.pdb %2 /D /Y
 rem echo %ERRORLEVEL%
 rem echo %1\..\bin\LoaderCmd.exe -l 1
 %1\..\bin\LoaderCmd.exe -l 1
