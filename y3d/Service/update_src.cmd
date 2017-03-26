d9e03265d8dea9dd82d0d478b85eb67230178afc
ducthangho <ducthangho@gmail.com>
2017-03-26 10:46:59 +0700

    - Update source grpc to version 1.2.0 - Libprotobuf to 3.2.0 officially in grpc -> remove unecessary script

--- y3d/Service/update_src.cmd
+++ y3d/Service/update_src.cmd
@@ -3,29 +3,29 @@ REM xcopy .gitmodules grpc /D /Y
 xcopy .gitignore grpc /D /Y
 cd grpc
 git fetch --all --tags --prune
-git checkout tags/v1.1.4
+git checkout tags/v1.2.0
 echo "grpc release tags: "
 git describe --tags
 git submodule update --init --remote --merge --recursive
-cd third_party\protobuf
-git fetch --all --tags --prune
-git checkout v3.2.0
-echo "protobuf release tags: "
-git describe --tags
+rem cd third_party\protobuf
+rem git fetch --all --tags --prune
+rem git checkout v3.2.0
+rem echo "protobuf release tags: "
+rem git describe --tags
 ) else (
-git clone --depth 1 --branch v1.1.4 --single-branch https://github.com/grpc/grpc.git
-git checkout tags/v1.1.4
+git clone --depth 1 --branch v1.2.0 --single-branch https://github.com/grpc/grpc.git
+git checkout tags/v1.2.0
 echo "grpc release tags: "
 git describe --tags
 REM xcopy .gitmodules grpc/ /D /Y
 xcopy .gitignore grpc /Y
 cd grpc
 git submodule update --init --remote --merge --recursive
-cd third_party\protobuf
-git fetch --all --tags --prune
-git checkout v3.2.0
-echo "protobuf release tags: "
-git describe --tags
+rem cd third_party\protobuf
+rem git fetch --all --tags --prune
+rem git checkout v3.2.0
+rem echo "protobuf release tags: "
+rem git describe --tags
 REM git config core.sparseCheckout true
 REM echo src/* >> .git/info/sparse-checkout
 REM echo include/* >> .git/info/sparse-checkout
